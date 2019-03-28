fc1670d9f7b8b05b5338f76e2b8397e9c13e8d29
bekabulia <bekabulia2008@yahoo.com>
2019-03-28 21:00:05 +0400

    setting up heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const hbs= require('hbs');
 const fs= require('fs');
-
+const port=process.env.PORT || 3000;
 
 
 
@@ -61,6 +61,6 @@ app.get('/bad',(req,res)=>{
     });
 })
 
-app.listen(3000,()=>{
-    console.log('server is starting');
+app.listen(port,()=>{
+    console.log(`server is running on ${port} `);
 });
